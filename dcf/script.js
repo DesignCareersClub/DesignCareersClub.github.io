e3b9284ed9c4089ca04e85172aeb7f06b23d00e8
WhatIsACore <zekixu@gmail.com>
2021-01-31 17:55:17 -0800

    final dcf updates

--- dcf/script.js
+++ dcf/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let revealButton = document.getElementById('reveal');
+revealButton.addEventListener('click', () => {
+  revealButton.style.display = 'none';
+  document.getElementById('timeline').style.display = 'flex';
+
+  setTimeout(() => {
+    for (let e of document.getElementsByClassName('timeline-event'))
+      e.style.opacity = 1;
+  }, 50);
+})
