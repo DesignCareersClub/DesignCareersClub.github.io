0a1163ee26f9a87c11378c75ffa60c7e224162a9
Annabel Koshy <ackoshy@ucdavis.edu>
2021-05-03 00:47:47 -0700

    media queries annabel

--- alumni-day/script.js
+++ alumni-day/script.js
@@ -1,1 +1,9 @@
-'use strict';
+/*  (function(){
+    'use strict'; 
+    document.querySelector('.see_details').addEventListener('click',function(event){
+        event.preventDefault(); 
+        document.getElementById('existing').className='hidden'; 
+        document.getElementById('overlay').className='showing'; 
+
+    });
+})(); */
